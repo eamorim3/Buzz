78aedee283c4734b158c9e4cf560bd79740b0f5c
eamorim3 <102839095+eamorim3@users.noreply.github.com>
2022-04-02 23:28:09 -0400

    Add files via upload

---
+++ BRASAHacks/src/pages/SignUpForm.js
@@ -0,0 +1,80 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+
+class SignUpForm extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      email: "",
+      password: "",
+      name: "",
+      hasAgreed: false
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    let target = event.target;
+    let value = target.type === "checkbox" ? target.checked : target.value;
+    let name = target.name;
+
+    this.setState({
+      [name]: value
+    });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+
+    console.log("The form was submitted with the following data:");
+    console.log(this.state);
+  }
+
+  render() {
+    return (
+      <div className="formCenter">
+        <form onSubmit={this.handleSubmit} className="formFields">
+          <div className="formField">
+            <label className="formFieldLabel" htmlFor="name">
+              Age
+            </label>
+            <input
+              type="text"
+              id="name"
+              className="formFieldInput"
+              placeholder="Enter your age"
+              name="name"
+              value={this.state.name}
+              onChange={this.handleChange}
+            />
+          </div>
+          <div className="formField">
+            <label className="formFieldLabel" htmlFor="password">
+              job occupation
+            </label>
+            <input
+              type="password"
+              id="password"
+              className="formFieldInput"
+              placeholder="Enter your job occupation"
+              name="password"
+              value={this.state.password}
+              onChange={this.handleChange}
+            />
+          </div>
+
+          <div className="formField">
+            <button className="formFieldButton">Sign Up</button>{" "}
+            <Link to="/sign-in" className="formFieldLink">
+              I'm already member
+            </Link>
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+export default SignUpForm;
